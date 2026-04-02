34fcd2fdff278e80c891a31960a93993fa18f568
CK <CK@MSI>
2026-04-02 15:15:47 -0300

    Add project files.

---
+++ WorkOrderApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WorkOrderApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
