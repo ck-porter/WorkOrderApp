fc099874460437df27095e7f2ddebf3ce909a6ea
CK Porter <ck.porter@outlook.com>
2026-04-02 15:55:18 -0300

    Add migrations and intial db config

---
+++ WorkOrderApp/Data/WorkOrderAppContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WorkOrderApp.Data
+{
+    public class WorkOrderAppContext : DbContext
+    {
+        //constructor
+        public WorkOrderAppContext(DbContextOptions<WorkOrderAppContext> options) : base(options) { }
+
+        //instances
+        DbSet<Models.Employee> Employees { get; set; }
+        DbSet<Models.WorkOrder> WorkOrders { get; set; }
+
+    }
+}
---
+++ WorkOrderApp/Models/WorkOrder.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WorkOrderApp.Models
+{
+    public class WorkOrder
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; } = null!;
+        [Required]
+        public string Description { get; set; } = null!;
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        [Required]
+        public string Status { get; set; } = "Open"; // Default status is Open
+        public int? AssignedToId { get; set; }
+        public Employee? AssignedTo { get; set; }
+    }
+}
