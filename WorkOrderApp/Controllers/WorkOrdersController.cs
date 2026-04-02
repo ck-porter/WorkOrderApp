49a6021491843a94022b200b65b2b70cd15db16e
CK Porter <ck.porter@outlook.com>
2026-04-02 16:55:09 -0300

    Added controllers and views for employees and work orders

---
+++ WorkOrderApp/Controllers/WorkOrdersController.cs
@@ -0,0 +1,164 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using WorkOrderApp.Data;
+using WorkOrderApp.Models;
+
+namespace WorkOrderApp.Controllers
+{
+    public class WorkOrdersController : Controller
+    {
+        private readonly WorkOrderAppContext _context;
+
+        public WorkOrdersController(WorkOrderAppContext context)
+        {
+            _context = context;
+        }
+
+        // GET: WorkOrders
+        public async Task<IActionResult> Index()
+        {
+            var workOrderAppContext = _context.WorkOrders.Include(w => w.AssignedTo);
+            return View(await workOrderAppContext.ToListAsync());
+        }
+
+        // GET: WorkOrders/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var workOrder = await _context.WorkOrders
+                .Include(w => w.AssignedTo)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (workOrder == null)
+            {
+                return NotFound();
+            }
+
+            return View(workOrder);
+        }
+
+        // GET: WorkOrders/Create
+        public IActionResult Create()
+        {
+            ViewData["AssignedToId"] = new SelectList(_context.Employees, "Id", "Email");
+            return View();
+        }
+
+        // POST: WorkOrders/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Title,Description,CreatedAt,Status,AssignedToId")] WorkOrder workOrder)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(workOrder);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["AssignedToId"] = new SelectList(_context.Employees, "Id", "Email", workOrder.AssignedToId);
+            return View(workOrder);
+        }
+
+        // GET: WorkOrders/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var workOrder = await _context.WorkOrders.FindAsync(id);
+            if (workOrder == null)
+            {
+                return NotFound();
+            }
+            ViewData["AssignedToId"] = new SelectList(_context.Employees, "Id", "Email", workOrder.AssignedToId);
+            return View(workOrder);
+        }
+
+        // POST: WorkOrders/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Title,Description,CreatedAt,Status,AssignedToId")] WorkOrder workOrder)
+        {
+            if (id != workOrder.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(workOrder);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!WorkOrderExists(workOrder.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["AssignedToId"] = new SelectList(_context.Employees, "Id", "Email", workOrder.AssignedToId);
+            return View(workOrder);
+        }
+
+        // GET: WorkOrders/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var workOrder = await _context.WorkOrders
+                .Include(w => w.AssignedTo)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (workOrder == null)
+            {
+                return NotFound();
+            }
+
+            return View(workOrder);
+        }
+
+        // POST: WorkOrders/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var workOrder = await _context.WorkOrders.FindAsync(id);
+            if (workOrder != null)
+            {
+                _context.WorkOrders.Remove(workOrder);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool WorkOrderExists(int id)
+        {
+            return _context.WorkOrders.Any(e => e.Id == id);
+        }
+    }
+}
